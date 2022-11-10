12ec2a4a6eebd18f1fe944c5000619117431c877
donovanj44 <josephddonovan345@gmail.com>
2022-11-10 13:14:41 -0500

    Add all dependencies and edit Webhook.java a bit

--- Webhook.java
+++ Webhook.java
@@ -1,11 +1,53 @@
-import java.io.IOException;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.forms.v1.Forms;
 
-public class Webhook extends privates {
-    public static void main(String []args) throws IOException{
-        
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Objects;
+
+import com.google.api.services.forms.v1.FormsScopes;
+import com.google.auth.oauth2.GoogleCredentials;
+public class Webhook extends Privates {
+
+    private static final String APPLICATION_NAME = "google-form-api-project";
+    private static Drive driveService;
+    private static Forms formsService;
+
+    static {
+
+        try {
+
+            JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+
+            driveService = new Drive.Builder(GoogleNetHttpTransport.newTrustedTransport(),
+                    jsonFactory, null)
+                    .setApplicationName(APPLICATION_NAME).build();
+
+            formsService = new Forms.Builder(GoogleNetHttpTransport.newTrustedTransport(),
+                    jsonFactory, null)
+                    .setApplicationName(APPLICATION_NAME).build();
+
+        } catch (GeneralSecurityException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
         DiscordWebhook webhook = new DiscordWebhook(url);
         webhook.setAvatarUrl(avatarUrl);
         webhook.setContent("Test");
         webhook.execute();
+    }
+
+    public static String getAccessToken() throws IOException {
+        GoogleCredentials credential = GoogleCredentials.fromStream(Objects.requireNonNull(
+                Webhook.class.getResourceAsStream("cred.json"))).createScoped(FormsScopes.all());
+        return credential.getAccessToken() != null ?
+                credential.getAccessToken().getTokenValue() :
+                credential.refreshAccessToken().getTokenValue();
     }
 }
