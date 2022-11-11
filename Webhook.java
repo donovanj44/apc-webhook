b12a018dd803857e7d70f894f9e542461354a345
donovanj44 <josephddonovan345@gmail.com>
2022-11-11 09:38:13 -0500

    reading responses works 🥳

--- Webhook.java
+++ Webhook.java
@@ -1,13 +1,13 @@
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.services.drive.Drive;
-import com.google.api.services.drive.model.Permission;
-import com.google.api.services.drive.model.PermissionList;
 import com.google.api.services.forms.v1.Forms;
 import com.google.api.services.forms.v1.FormsScopes;
 import com.google.api.services.forms.v1.model.*;
 import com.google.auth.oauth2.GoogleCredentials;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
@@ -16,8 +16,9 @@ import java.util.*;
 
 public class Webhook extends Privates {
 
+    final String question1Id = "059d7375", question2Id = "106fb4ba", question3Id = "01f8242e", question4Id = "69fbc838";
+
     private static final String APPLICATION_NAME = "apc-webhook";
-    private static Drive driveService;
     private static Forms formsService;
 
 
@@ -27,9 +28,6 @@ public class Webhook extends Privates {
 
             JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
 
-            driveService = new Drive.Builder(GoogleNetHttpTransport.newTrustedTransport(),
-                    jsonFactory, null)
-                    .setApplicationName(APPLICATION_NAME).build();
 
             formsService = new Forms.Builder(GoogleNetHttpTransport.newTrustedTransport(),
                     jsonFactory, null)
@@ -40,12 +38,9 @@ public class Webhook extends Privates {
         }
     }
 
-    public static void main(String[] args) throws IOException, GeneralSecurityException {
+    public static void main(String[] args) throws IOException {
 
-        String token = token = getAccessToken();
-        String formID = createNewForm(token), responseID = "ACYDBNhRt_yvBT58aLZvybdmUCWQkyldz" +
-                "Yun-d_LQ15naDKMovqMlal1hhstVvEMaA9CBBw";
-        publishForm(formID,token);
+        String token = getAccessToken();
         readResponses(formID, token);
         DiscordWebhook webhook = new DiscordWebhook(url);
         webhook.setAvatarUrl(avatarUrl);
@@ -61,33 +56,15 @@ public class Webhook extends Privates {
                 credential.refreshAccessToken().getTokenValue();
     }
 
-    private static String createNewForm(String token) throws IOException {
-        Form form = new Form();
-        form.setInfo(new Info());
-        form.getInfo().setTitle("New Form Quiz Created from Java");
-        form = formsService.forms().create(form)
-                .setAccessToken(token)
-                .execute();
-        return form.getFormId();
-    }
-
-    public static boolean publishForm(String formId, String token) throws GeneralSecurityException, IOException {
-
-        PermissionList list = driveService.permissions().list(formId).setOauthToken(token).execute();
-
-        if (!list.getPermissions().stream().filter((it) -> it.getRole().equals("reader")).findAny().isPresent()) {
-            Permission body = new Permission();
-            body.setRole("reader");
-            body.setType("anyone");
-            driveService.permissions().create(formId, body).setOauthToken(token).execute();
-            return true;
-        }
-
-        return false;
-    }
 
     private static void readResponses(String formId, String token) throws IOException {
-        ListFormResponsesResponse response = formsService.forms().responses().list(formId).setOauthToken(token).execute();
+        ListFormResponsesResponse responses = formsService.forms().responses().list(formId).setOauthToken(token).execute();
+        String responseSTR = String.valueOf(formsService.forms().responses().list(formId).setOauthToken(token).execute());
+        JSONObject json = new JSONObject(responses);
+        JSONArray arr = json.getJSONArray("responses");
+        String responseID = arr.getJSONObject(0).getString("responseId");
+        System.out.println(responses.toPrettyString());
+        FormResponse response = formsService.forms().responses().get(formId, responseID).setOauthToken(token).execute();
         System.out.println(response.toPrettyString());
     }
 }
