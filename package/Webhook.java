7843a4d1e6a6c1af56353fdb4f5b1b0cf3c2f3a1
donovanj44 <josephddonovan345@gmail.com>
2022-11-16 10:05:39 -0500

    only get latest response 🥳

--- package/Webhook.java
+++ package/Webhook.java
@@ -14,9 +14,9 @@ import org.rapidoid.setup.*;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.time.format.DateTimeFormatter;
 import java.util.Objects;
-
-
+import java.time.LocalDateTime;
 
 
 public class Webhook extends Privates {
@@ -24,6 +24,12 @@ public class Webhook extends Privates {
     final String question1Id = "059d7375", question2Id = "106fb4ba", question3Id = "01f8242e", question4Id = "69fbc838";
 
     String name, college, major;
+
+    public static LocalDateTime dateTime = LocalDateTime.parse("2017-01-14T15:32:56.000");
+    public static LocalDateTime checkLDT;
+
+    public static FormResponse check;
+
 
     private static final String APPLICATION_NAME = "apc-webhook";
     private static Forms formsService;
@@ -46,23 +52,14 @@ public class Webhook extends Privates {
     }
 
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         On.get("/fire").html((Req req) -> {
             Resp resp = req.response();
             fireWebhook();
-            return "resp";
+            return "fired";
         });
-        On.post("/fire").html((Req req) -> {
-            Resp resp = req.response();
-            fireWebhook();
-            return "resp";
-        });
-
 
     }
-
-
-
 
 
     public static void fireWebhook() throws IOException {
@@ -72,7 +69,7 @@ public class Webhook extends Privates {
         DiscordWebhook webhook = new DiscordWebhook(Privates.url);
         webhook.setAvatarUrl(Privates.avatarUrl);
         webhook.setContent("Test");
-        webhook.execute();
+//        webhook.execute();
     }
 
     public static String getAccessToken() throws IOException {
@@ -84,7 +81,7 @@ public class Webhook extends Privates {
     }
 
     public static void readResponses(String formId, String token) throws IOException {
-        ListFormResponsesResponse responses = formsService.forms().responses().list(formId).setOauthToken(token).execute();
+        ListFormResponsesResponse responses = formsService.forms().responses().list(formId).setOauthToken(getAccessToken()).execute();
         JSONObject goodJson = new JSONObject(responses);
         JSONArray arr = goodJson.getJSONArray("responses");
         String responseID = arr.getJSONObject(arr.length() - 1).getString("responseId");
@@ -92,7 +89,19 @@ public class Webhook extends Privates {
         FormResponse response = formsService.forms().responses().get(formId, responseID).setOauthToken(token).execute();
 //        System.out.println(response.toPrettyString());
         JSONObject responseJson = new JSONObject(response);
-
-
+        for (int i = 0; i < arr.length(); i++){
+            check = formsService.forms().responses().get(formId,responseID).setOauthToken(token).execute();
+            checkLDT = LocalDateTime.parse(arr.getJSONObject(i).getString("lastSubmittedTime").substring(0, arr.getJSONObject(i).getString("lastSubmittedTime").length() - 3));
+            if(checkLDT.isAfter(dateTime)){
+                dateTime = checkLDT;
+//                System.out.println(dateTime);
+            }
+        }
+        ListFormResponsesResponse returnedResponse = formsService.forms().responses().list(formId).setOauthToken(getAccessToken()).setFilter("timestamp >= " + dateTime.toString() + "Z").execute();
+        System.out.println(returnedResponse.toPrettyString());
+        JSONObject returnedJSON = new JSONObject(returnedResponse);
+        JSONArray arr2 = returnedJSON.getJSONArray("responses");
+        JSONArray arr3 = arr2.getJSONArray(1);
     }
+
 }
